7372ac854c57f10b152aa7bdef836bdbbae21ac2
apllnrrr <polinabliznyk@gmail.com>
2024-08-14 11:35:36 +0300

    feat: make a fun in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  const str = n.toString()
+  let res = ''
+  for(let i =str.length -1 ; i>=0; i--){
+    if(str[i]!='-'){
+        let index = i
+        res +=str[index]
+    }
+  }
+   const result = Number(res)
+   return result
 }
+
